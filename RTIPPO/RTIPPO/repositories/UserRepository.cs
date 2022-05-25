60a6f5ef0e5277f872726ea18f4686f390128967
MAlyaV <alyamagz@gmail.com>
2022-05-25 21:48:19 +0500

    Lina25/05

--- RTIPPO/RTIPPO/repositories/UserRepository.cs
+++ RTIPPO/RTIPPO/repositories/UserRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace RTIPPO.repositories
 {
-    class UserRepository
+    public class UserRepository
     {
 
         private bool auth=false;
@@ -19,7 +19,7 @@ namespace RTIPPO.repositories
             }
             else
             {
-                //DataBase base = new DataBase('select ');
+
                 auth =true;
                 return true;
                 
@@ -35,6 +35,21 @@ namespace RTIPPO.repositories
             }
             else
             {
+                DataBase db = new DataBase("select login from users");
+                foreach (DataRow row in db.data.Rows)
+                {
+                    var cells = row.ItemArray;
+                    foreach (object cell in cells)
+                    {
+                        Console.Write("\t{0}", cell);
+                        if (cell.ToString().Trim() == login.Trim())
+                        {
+                            return "Пользователь с таким логином уже существует";
+                        }
+                    }
+                }
+                new DataBase("INSERT INTO public.users(id_location, password, login, id_status) VALUES(0, '"+password+"', '"+login+"', 0)");
+                auth= true;
                 return "";
             }
         }
